f8624d56eefc26e11f6570d2e78016ffff542209
Carl Gray <carl@firstfnf.com>
2019-03-04 12:09:58 +1100

    simplify code av css

--- src/components/EpisodeProfile.vue
+++ src/components/EpisodeProfile.vue
@@ -8,14 +8,15 @@
         
       </div>
       <div>
-        <progress @click="progressClick" id="seekbar" value="0.1" max="1"></progress>
+        <progress ref="progressref" @click="progressClick" id="seekbar" value="0.1" max="1"></progress>
       </div>
     </div>
     <div class="profile-container">
       
       <div class="profile-content-player">
         <div class="audiovisualizer">
-                <div class="changeheight0" :style="{height: data1[0]+'px'}"></div>
+                <div v-for="(item, index) in data1" class="changeheight" :style="{height: data1[index]+'px'}" :key="index"></div>
+                <!-- <div class="changeheight0" :style="{height: data1[0]+'px'}"></div>
                 <div class="changeheight1" :style="{height: data1[1]+'px'}"></div>
                 <div class="changeheight2" :style="{height: data1[2]+'px'}"></div>
                 <div class="changeheight3" :style="{height: data1[3]+'px'}"></div>
@@ -23,19 +24,13 @@
                 <div class="changeheight5" :style="{height: data1[5]+'px'}"></div>
                 <div class="changeheight6" :style="{height: data1[6]+'px'}"></div>
                 <div class="changeheight7" :style="{height: data1[7]+'px'}"></div>
-                <div class="changeheight8" :style="{height: data1[8]+'px'}"></div>
+                <div class="changeheight8" :style="{height: data1[8]+'px'}"></div> -->
 
         </div>
-        <!-- <audio
-          :src="episodeToPlay"
-          autoplay
-          controls
-          crossorigin="anonymous"
-        ></audio> -->
         <p>{{ currentProgress }} / {{ totalDuration }}</p>
         <div class="audio-player">
-          <audio 
-            :ontimeupdate.prop="udpateProgress" 
+          <audio ref="playerref"
+            :ontimeupdate.prop="updateProgress" 
             id="player"
             :src="episodeToPlay"
             autoplay
@@ -70,8 +65,10 @@ export default {
       currentProgress: 0,
       totalDuration: 0,
       speed: null,
-      data1: [100, 100, 50, 100, 50, 150, 150, 50, 90],
-      boxposition: null
+      data1: null,
+      boxposition: null,
+      analyser1: null,
+      analyser2: null
     };
   },
   watch: {
@@ -91,6 +88,7 @@ export default {
   },
   methods: {
     async getSoundCloud() {
+      this.data1 = null;
       const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
       const response = await axios.get(
         `https://api.soundcloud.com/users/235518337/tracks?client_id=${CLIENT_ID}&limit=10&linked_partitioning=0&offset=0`
@@ -102,11 +100,8 @@ export default {
       this.episodeToPlay = `${result.stream_url +
         "?client_id=" + CLIENT_ID}`;
 
-    var audioElement = document.getElementById('player');
-    audioElement.src = `${result.stream_url +
-        "?client_id=" + CLIENT_ID}`
-    // audioElement.src = require('../assets/SampleAudio.mp3');
-    audioElement.crossOrigin = "anonymous";
+    // webaudio API;
+    var audioElement = this.$refs.playerref
 
     var ctx = new AudioContext();
     var sourceNode = ctx.createMediaElementSource(audioElement);
@@ -116,83 +111,73 @@ export default {
     analyser.fftSize = 1024;
     sourceNode.connect(analyser);
     analyser.connect(ctx.destination);
+    this.analyser1 = analyser
 
-    audioElement.play()
+    // audioElement.play()
 
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    // analyser.frequencyBinCount
+    // var frequencyData = new Uint8Array(128);
+    // // analyser.frequencyBinCount
+    // analyser.getByteFrequencyData(frequencyData);
+    // this.data1 = frequencyData;
 
-    analyser.getByteFrequencyData(frequencyData);
-
-    this.data1 = frequencyData;
-
-    // console.log(frequencyData);
-
-      setInterval(function() {
-        analyser.getByteFrequencyData(frequencyData);
-        // console.log(frequencyData);
-    //     function groupAverage(arr, n) {
-    //   var result = [];
-    //   for (var i = 0; i < arr.length;) {
-    //    var sum = 0;
-    //    for(var j = 0; j< n; j++){
-    //   // Check if value is numeric. If not use default value as 0
-    //     sum += +arr[i++] || 0
-    //    }
-    //    result.push(sum/n);
-    //  }
-    //    return result
-      // }
-        // console.log(result);
-        this.data1 = frequencyData;
-        console.log(this.data1)
-      }, 1000);
+    //   setInterval(function() {
+    //     analyser.getByteFrequencyData(frequencyData);
+    //     this.data1 = frequencyData;
+    //     console.log(this.data1)
+    //   }, 1000);
 
 
 
 
     },
     play() {
-      document.getElementById('player').play();
+      this.$refs.playerref.play();
     },
     pause() {
-      document.getElementById('player').pause();
+      this.$refs.playerref.pause();
     },
     skip() {
-     let player = document.getElementById('player')
+     let player = this.$refs.playerref
       player.currentTime += 30.0;
     },
     rewind() {
-       let player = document.getElementById('player')
+       let player = this.$refs.playerref
       player.currentTime -= 30.0;
     },
     playbackSpeed() {
-       let player = document.getElementById('player')
+       let player = this.$refs.playerref
       player.playbackRate = 1.5;
     },
     normalSpeed() {
-       let player = document.getElementById('player')
+       let player = this.$refs.playerref
       player.playbackRate = 1;
     },
 
     progressClick(e) {
-      let el = document.getElementById('seekbar');
+      let el = this.$refs.progressref;
       var x = e.pageX - this.$parent.$refs['myref'].getBoundingClientRect().width
-      var startPos = document.getElementById('seekbar').position;
+      var startPos = this.$refs.progressref.position;
       console.log(this.$refs['myref2'].getBoundingClientRect().width);
       var xconvert = x/this.$refs['myref2'].getBoundingClientRect().width;
       var finalx = (xconvert).toFixed(1);
-      document.getElementById('seekbar').value = finalx
+      this.$refs.progressref.value = finalx
      
-      var player = document.getElementById('player');
+      var player = this.$refs.playerref;
       var finalseconds = xconvert*player.duration;
       player.currentTime = finalseconds;
 
     },
 
-    udpateProgress() {
-      const player = document.getElementById('player');
-      const progressbar = document.getElementById('seekbar');
+    updateProgress() {
+      
+      // code for webaudio api, rather than in setinterval loop
+      var frequencyData = new Uint8Array(256);
+      this.analyser1.getByteFrequencyData(frequencyData);
+      this.data1 = frequencyData;
+      
+      // code for updating progress value bar
+      const player = this.$refs.playerref;
+      const progressbar = this.$refs.progressref;
       progressbar.value = (player.currentTime / player.duration);
 
       const timeConvert = (num) => { 
@@ -318,79 +303,16 @@ button:hover::after {
   height: 100%;
 }
 
+.changeheight {
+  display: flex;
+  background-color: #5000FF;
+  /* border: 1px solid white; */
+  color: white;
+  width: 2px;
+  justify-items: center;
+}
+
 .changeheight0 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight1 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight2 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight3 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight4 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight5 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight6 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight7 {
-  display: flex;
-  background-color: #5000FF;
-  /* border: 1px solid white; */
-  color: white;
-  width: 20px;
-  justify-items: center;
-}
-
-.changeheight8 {
   display: flex;
   background-color: #5000FF;
   /* border: 1px solid white; */
