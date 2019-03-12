fa2a5aedb99a227298740d81a7fbd0e125783a8f
Carl Gray <carl@firstfnf.com>
2019-03-13 10:57:06 +1100

    dynamic search page 1

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,10 @@ export default {
    watch: {
     $route() {
       this.getSoundCloud();
-    }
+    },
+    inputChange: function(nv, ov) {
+      this.getSoundCloud()
+    },
   },
   computed: {
     filteredepisodes: function() {
@@ -98,7 +101,7 @@ export default {
      } else if (this.$route.params.podcastseries) {
           axios
         .get(
-          `https://listennotes.p.rapidapi.com/api/v1/search?sort_by_date=0&type=episode&offset=0&len_min=2&len_max=10&genre_ids=68%2C82&published_before=1490190241000&published_after=1390190241000&only_in=title&language=English&ocid=${this.$route.params.podcastseries}&safe_mode=1&q=${this.$route.params.series}`, {
+          `https://listennotes.p.rapidapi.com/api/v1/search?sort_by_date=0&type=episode&offset=0&len_min=2&len_max=10&genre_ids=68%2C82&published_before=1490190241000&published_after=1390190241000&only_in=title&language=English&ocid=${this.$route.params.podcastseries}&safe_mode=1&q=${this.inputChange || this.$route.params.series}`, {
             headers: {
               'X-RapidAPI-Key': `${CLIENT_ID_LISTEN}`
             }
